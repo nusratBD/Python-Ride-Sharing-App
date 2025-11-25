5a7dfeeab642756645c3b3987a49cff9927786b9
nusratBD <nusrat.hurain15@gmail.com>
2025-11-25 13:39:19 +0600

    User ride request update

--- User.py
+++ User.py
@@ -1,8 +1,9 @@
 """
-1.Create User
-2.Request Ride
+1.Create a user
+2.Request a ride
 3.Save Ride History
 """
+from Ride import Ride
 class User:
     def __init__(self,name,email,phone):
         self.name=name
@@ -12,8 +13,10 @@ class User:
     def request_ride(self,start_location,end_location):
         self.start_location=start_location
         self.end_location=end_location
-        from Ride import Ride
-        ride=Ride(self)
-        return ride
-    def user_ride_history(self,ride):
-        self.ride_history.append(ride)
+        self.ride=Ride(self)
+        return self.ride
+    def user_ride_history(self):
+        self.ride_history.append(self.ride)
+u1=User("Nusrat","nusrat@gmail.com","018476554")
+u1.request_ride("Banani","Mirpur")
+print(u1.__dict__)
